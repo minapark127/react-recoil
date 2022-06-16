25ac7836b57d4152620303dfce1cb9972da9e54c
Mina <mina@lashcreative.com.au>
2022-06-16 11:34:58 +1000

    [feat] update todos category on click

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,8 +1,27 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({text}:IToDo){
+function ToDo({text, category, id}:IToDo){
+    const setToDos = useSetRecoilState(toDoState);
+
+    const onClick = (newCategory:IToDo["category"]) => {
+        setToDos(oldToDos => {
+            const targetIndex = oldToDos.findIndex( toDo => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            const newToDo = {...oldToDo, category: newCategory};         
+            const newToDos = [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)];
+            return newToDos;
+        });
+    };
+
     return (
-        <li>{text}</li>
+        <li>
+            <span>{text}</span>
+            {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")} >To Do</button>}
+            {category !== "DOING" && <button onClick={() => onClick("DOING")} >Doing</button>}
+            {category !== "DONE" && <button onClick={() => onClick("DONE")} >Done</button>}
+        </li>
     );
 }
 
