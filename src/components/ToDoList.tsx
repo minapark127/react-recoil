807a7173ffd6c67898ab50f12c3942259ffee304
Mina <mina@lashcreative.com.au>
2022-06-16 12:33:53 +1000

    [feat] save and render todo per category select

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,16 +1,31 @@
-import { useRecoilValue } from 'recoil';
-import { toDoState } from '../atoms';
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { categoryState, toDoSelector, toDoState } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 
 
 function TODOList(){
-    const toDos = useRecoilValue(toDoState);
+    const toDos = useRecoilValue(toDoSelector);
+    const [category, setCategory] = useRecoilState(categoryState);
+
+    const onInput = (event:React.FormEvent<HTMLSelectElement>) => {
+        const { currentTarget: { value: newCategory}} = event;
+        setCategory(newCategory);
+    }
     return (
         <div>
             <h1>To Do List</h1>
+            <hr />
+
+            <select onInput={onInput}>
+                <option value="TO_DO">To Do</option>
+                <option value="DOING">Doing</option>
+                <option value="DONE">Done</option>
+            </select>
             <CreateToDo />
+            <hr />
 
             <ul>
                 {toDos.map(toDo => <ToDo key={toDo.id} {...toDo}/>)}
